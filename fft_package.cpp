4ad3d64e9ae6aedae6b881a3d421d6b06e018301
Leahlijuan <jl10889@nyu.edu>
2022-04-29 15:06:52 -0400

    add fftw

--- fft_package.cpp
+++ fft_package.cpp
@@ -8,9 +8,12 @@ int main(int argc, char **argv)
     long log2N = read_option<long>("-n", argc, argv);
     long times = 20;
     double total_time = 0.0;
-    for (int k = 0; k < times; k++)
+
+    for (int i = 1; i <= log2N; i++)
     {
-        for (int i = 1; i <= log2N; i++)
+        printf("N: %d\n", i);
+        double total_time = 0.0;
+        for (int k = 0; k < times; k++)
         {
             long N = pow(2, i);
             fftw_complex *in, *out, *back_out;
@@ -31,27 +34,23 @@ int main(int argc, char **argv)
             q = fftw_plan_dft_1d(N, out, back_out, FFTW_BACKWARD, FFTW_ESTIMATE);
             fftw_execute(p); /* repeat as needed */
             fftw_execute(q);
-            printf("N: %d\n", i);
+
             total_time += timer.toc();
             // printf("run time: %3f\n", timer.toc());
             double err = 0.0;
             for (int i = 0; i < N; i++)
             {
-                // printf("%3f %3f ", in[i][0], in[i][1]);
-                // printf("%3f %3f ", back_out[i][0], back_out[i][1]);
-                // printf("%3f ", in[i][0] - back_out[i][0]/N);
                 err += in[i][0] - back_out[i][0] / N;
             }
-            printf("error: %3f\n", err);
+            // printf("error: %3f\n", err);
             fftw_destroy_plan(p);
             fftw_destroy_plan(q);
             fftw_free(in);
             fftw_free(out);
             fftw_free(back_out);
         }
+        printf("run time: %3f\n", total_time / times);
     }
-    double avg_time = total_time / times;
-    printf("fftw run time: %3f", avg_time);
 
     return 0;
 }
